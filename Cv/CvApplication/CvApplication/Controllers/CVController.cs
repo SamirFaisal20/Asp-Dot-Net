5f585d4b703649712e2b7b6a48da36b65dc729bf
Samir Faisal <samir.faisalaiubcse@gmail.com>
2022-09-22 19:30:52 +0600

    First Lab Task : CV

---
+++ Cv/CvApplication/CvApplication/Controllers/CVController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvApplication.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Name()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Skl()
+        {
+            return View();
+        }
+    }
+}
